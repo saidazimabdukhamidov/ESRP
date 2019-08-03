02bf503a83cc663b3136a08dc2d99250e69af076
saidazimabdukhamidov <saidazimabduhamidov@gmail.com>
2019-08-03 17:28:17 +0500

    Add files via upload

---
+++ src/main/java/psb/esrp/EsrpApplication.java
@@ -0,0 +1,11 @@
+package psb.esrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsrpApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EsrpApplication.class, args);
+  }
+}
---
+++ src/test/java/psb/esrp/EsrpApplicationTests.java
@@ -0,0 +1,16 @@
+package psb.esrp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsrpApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
