9d5c37911db2bb5203f5ce38826f77ed472edc4a
Saidazim <saidazimabduhamidov@gmail.com>
2019-08-05 12:00:03 +0500

    adding DB class

--- src/main/java/psb/esrp/utils/DB.java
+++ src/main/java/psb/esrp/utils/DB.java
@@ -1,4 +1,37 @@
 package psb.esrp.utils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class DB {
+  public static void done(Connection obj) {
+    try {
+      if (obj != null) {
+        obj.close();
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static void done(PreparedStatement obj) {
+    try {
+      if (obj != null) {
+        obj.close();
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static void done(ResultSet obj) {
+    try {
+      if (obj != null) {
+        obj.close();
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
 }
